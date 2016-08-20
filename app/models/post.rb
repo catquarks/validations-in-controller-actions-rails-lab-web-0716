b0ac6c77dc45b1b2d81bbe1944a8791ff76fa855
Tami <catquarks@gmail.com>
2016-08-20 17:22:44 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 100}
+	validate :correct_category
+
+
+	private
+		def correct_category
+			valid_categories = ["fiction", "non-fiction"]
+			if category 
+				unless valid_categories.include?(category.downcase)
+					errors.add(:category, "must be a valid category! e.g. #{valid_categories.join(", ")}")
+				end
+			else
+				errors.add(:category, "must exist! e.g. #{categories.join(", ")}")
+			end
+		end
 end
